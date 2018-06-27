601097132738f562fc7709975fa71d13973f0a16
tomcat111111 <35770075+tomcat111111@users.noreply.github.com>
2018-06-27 11:11:02 +0900

    Update ContactController.php
    お問い合わせのメール処理改善

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -13,72 +13,77 @@ use Illuminate\Support\Facades\Auth;
 
 class ContactController extends Controller
 {
-    public function contact(Request $request){
+  public function contact(Request $request){
 		
-		if( $request->input("action") == "応募" ) {
+    if( $request->input("action") == "送信" ) {
 			
-			$rules = [
-				'name' => 'required',
-				'email' => 'required | email',
-				'content' => 'required',
-			];
+      rules = [
+	'name' => 'required',
+	'email' => 'required | email',
+	'content' => 'required',
+	];
+
+      //バリデーションのメッセージ文をカスタマイズ		
+      $messages = [
+	'name.required' => 'お名前は必ず入力してください。',
+	'email.required' => 'メールアドレスは必ず入力してください。',
+	'email.email' => 'メールアドレスを正しい形式で入力してください',
+	'content.required' => 'お問い合わせ内容は必ず入力してください。',
+	];
 			
-			$messages = [
-				'name.required' => 'お名前は必ず入力してください。',
-				'email.required' => 'メールアドレスは必ず入力してください。',
-				'email.email' => 'メールアドレスを正しい形式で入力してください',
-				'content.required' => 'お問い合わせ内容は必ず入力してください。',
-			];
+      $validator = Validator::make($request->all(), $rules, $messages);
+      $errors = $validator->errors();
 			
-			$validator = Validator::make($request->all(), $rules, $messages);
-			$errors = $validator->errors();
+      $request->flash();
 			
-			$request->flash();
+      if($validator->fails()) {
+	return redirect('contact')->withErrors($validator)->withInput();
+      }
 			
-			if($validator->fails()) {
-				return redirect('contact')->withErrors($validator)->withInput();
-			}
+      $name = $request->input('name');
+      $email = $request->input('email');
+      $content = $request->input('content');
+      $date = date("Y/m/d H:i:s");
+      $delete_flag = 0;
+	    
+      //バリデーションに成功したら問い合わせ内容をDBに保存
 			
-			$name = $request->input('name');
-			$email = $request->input('email');
-			$content = $request->input('content');
+      DB::table('contacts')->insert([
+        'name' => $name,
+        'email' => $email,
+        'content' => $content,
+        'created_at' => $date,
+        'delete_flag' => $delete_flag,
+        ]);
+	    
+      //問い合わせをした人に確認のメール送信
 			
-			$date = date("Y/m/d H:i:s");
-			$delete_flag = 0;
+      \Mail::send(new \App\Mail\Contact([
+        'to' => $email,
+        'to_name' => $name,
+        'from' => 'hoge@example.com',
+        'from_name' => 'HogeHoge',
+        'subject' => 'お問い合わせありがとうございます',
+        'name' => $name,
+	'email' => $email,
+        'content' => $content,
+        ], 'contact'));
+	    
+      //運営側に問い合わせを通知するメール送信
 			
-            DB::table('applicants')->insert([
-                'name' => $name,
-                'email' => $email,
-                'content' => $content,
-                'created_at' => $date,
-                'delete_flag' => $delete_flag,
-            ]);
+      \Mail::send(new \App\Mail\Contact([
+        'to' => 'estedejapon999@yahoo.co.jp',
+        'to_name' => 'SmartFunding',
+        'from' => $email,
+        'from_name' => $name,
+        'subject' => 'お問い合わせがありました',
+        'name' => $name,
+        'email' => $email,
+	'content' => $content,
+        ], 'contactfrom'));
 			
-		    \Mail::send(new \App\Mail\Contact([
-                'to' => $email,
-                'to_name' => $name,
-                'from' => 'estedejapon999@yahoo.co.jp',
-                'from_name' => 'SmartFunding',
-                'subject' => 'お問い合わせありがとうございます',
-                'name' => $name,
-				'email' => $email,
-				'content' => $content,
-            ], 'contact'));
-			
-			\Mail::send(new \App\Mail\Contact([
-                'to' => 'estedejapon999@yahoo.co.jp',
-                'to_name' => 'SmartFunding',
-                'from' => $email,
-                'from_name' => $name,
-                'subject' => 'お問い合わせがありました',
-                'name' => $name,
-				'email' => $email,
-				'content' => $content,
-            ], 'contactfrom'));
-			
-			return redirect("contact/thanks");
-		}
-		
-		return view("contact");
-	}
+      return redirect("contact/thanks");
+    }
+    return view("contact");
+  }
 }
